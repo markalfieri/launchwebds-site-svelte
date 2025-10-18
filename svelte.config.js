b30523c942f2ac5a0133644c910bfbd1583cc46f
markalfieri2 <markalfieri@usf.edu>
2025-10-18 01:55:51 -0400

    Fixed sveltekit issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,7 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   kit: {
     adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+  }
 };
-
-export default config;
